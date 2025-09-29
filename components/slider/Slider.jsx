f9fec7cbcb1139f8c4bfbd4ce06f7970538d40aa
nemeskalito <borisenko.igor2021@yandex.ru>
2025-09-29 14:15:56 +0300

    continue

--- components/slider/Slider.jsx
+++ components/slider/Slider.jsx
@@ -1,25 +1,52 @@
+'use client'
+
+import { useState } from 'react'
 import './slider.css'
 
-import slides from './slides'
+import slidesData from './slides'
 
-const SlideItem = ({ data }) => {
+const SlideItem = ({ data, handler }) => {
 	return (
 		<li className='slide-list__item'>
-			<button className='button'>
-				<img src='./img/icons/plus.svg' alt='#' />
-				{data.title}
+			<button onClick={() => handler(data.id)} className='button'>
+				{data.isActive ? (
+					<>
+						<span>
+							<strong>{data.title}.</strong> {data.desc}
+						</span>
+					</>
+				) : (
+					<>
+						<img className='button__icon' src='./img/icons/plus.svg' alt='#' />
+						<strong>{data.title}</strong>
+					</>
+				)}
 			</button>
 		</li>
 	)
 }
 
 const Slider = () => {
+	const [slides, setSlides] = useState(slidesData)
+
+	const clickHandler = id => {
+		console.log(id)
+		setSlides(prev => {
+			return prev.map(slide => {
+				let isActive = false
+				if (slide.id === id) isActive = true
+
+				return { ...slide, isActive: isActive }
+			})
+		})
+	}
+
 	return (
 		<div className='slider'>
 			<div className='controls'>
 				<ul className='slide-list'>
 					{slides.map((slide, index) => (
-						<SlideItem data={slide} key={index} />
+						<SlideItem handler={clickHandler} data={slide} key={index} />
 					))}
 				</ul>
 			</div>
--- components/slider/slides.js
+++ components/slider/slides.js
@@ -1,35 +1,42 @@
 export default [
 	{
+		id: 1,
 		title: 'Colors',
 		desc: 'Choose from three bold finishes. iPhone 17 Pro shown in Deep Blue.',
-		isActive: false,
+		isActive: true,
 	},
 	{
+		id: 2,
 		title: 'Aluminum unibody',
 		desc: 'Optimized for performance and battery. Aluminum alloy is remarkably light and has exceptional thermal conductivity.',
 		isActive: false,
 	},
 	{
+		id: 3,
 		title: 'Vapor chamber',
 		desc: 'Deionized water sealed inside moves heat away from the A19 Pro chip, allowing for even higher sustained performance.',
 		isActive: false,
 	},
 	{
+		id: 4,
 		title: 'Ceramic Shield',
 		desc: 'Protects the back of iPhone 17 Pro, making it 4x more resistant to cracks.4 New Ceramic Shield 2 on the front has 3x better scratch resistance.5',
 		isActive: false,
 	},
 	{
+		id: 5,
 		title: 'Immersive pro display',
 		desc: 'Our best‑ever 6.3‑inch and 6.9‑inch Super Retina XDR displays.6 Brighter. Better anti‑reflection. ProMotion up to 120Hz.',
 		isActive: false,
 	},
 	{
+		id: 6,
 		title: 'Camera Control',
 		desc: 'Instantly take a photo, record video, adjust settings, and more. So you never miss a moment.',
 		isActive: false,
 	},
 	{
+		id: 7,
 		title: 'Action button',
 		desc: 'A customizable fast track to your favorite feature. Long press to launch the action you want — Silent mode, Translation, Shortcuts, and more.',
 		isActive: false,
